e77ae73ac023d164579f2b66fd082bbd67dfffee
zaki-yama <shingoyamazaki00@gmail.com>
2016-11-16 01:48:54 +0900

    introduce TextField and RaisedButton

--- src/components/AddTodoForm.js
+++ src/components/AddTodoForm.js
@@ -1,25 +1,43 @@
 import React, { PropTypes } from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
 export default class AddTodoForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      input: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      input: e.target.value,
+    });
+  }
+
   handleSubmit(e) {
     e.preventDefault();
-    const node = this.refs.input;
-    const text = node.value.trim();
+    const text = this.state.input.trim();
     if (!text) {
       return;
     }
     this.props.onSubmit(text);
-    node.value = '';
+    this.setState({
+      input: '',
+    });
   }
 
   render() {
     return (
       <div>
         <form onSubmit={(e) => this.handleSubmit(e)}>
-          <input ref="input" />
-          <button type="submit">
-            Add Todo
-          </button>
+          <TextField id="todo-title" value={this.state.input} onChange={this.handleChange} />
+          <RaisedButton label="Add Todo" onTouchTap={this.handleSubmit} />
         </form>
       </div>
     );
