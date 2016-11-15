c66162d1458166b21b139b331310a9db4e3d59f9
zaki-yama <shingoyamazaki00@gmail.com>
2016-11-16 01:16:39 +0900

    load Roboto font

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 import todoApp from './reducers';
 import App from './containers/App';
 
+import './main.css';
+
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
