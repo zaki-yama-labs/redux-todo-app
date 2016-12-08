d15ce8641b1d985ce957488e872209c0fae87842
zaki-yama <shingoyamazaki00@gmail.com>
2016-12-09 05:42:41 +0900

    Paperを使い、中央寄せで表示

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Paper from 'material-ui/Paper';
 
 import AddTodo from './AddTodo';
 import VisibleTodoList from './VisibleTodoList';
 import FilterLinkList from '../components/FilterLinkList';
 
+
 export default class App extends React.Component {
   render() {
     return (
       <MuiThemeProvider>
-        <div>
+        <Paper zDepth={2} className="todoapp">
           <AddTodo />
           <VisibleTodoList />
           <FilterLinkList />
-        </div>
+        </Paper>
       </MuiThemeProvider>
     );
   }
